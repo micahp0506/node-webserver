d3f9d734757c102d80e9917dee1186edd60806fb
Micah Wells <micahp0506@gmail.com>
2016-02-02 15:08:22 -0600

    Intial commit wit working app

--- server.js
+++ server.js
@@ -1,18 +1,57 @@
 'use strict'
 
-const http = require('http');
-const { PORT } = process.env;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
+app.get('/hello', (req, res) => {
 
+  const name = req.query.name;
+  const msg = `<h1>Hello ${name}!!!!!</h1>`;
+  console.log('Query PARAAMS', req.query);
   res.writeHead(200, {
-    'Content-type': 'text/html'
-  })
 
-  debugger;
+    'Content-Type': 'text/html'
 
-  res.end('<<h1>Heyzzz</h1>');
-}).listen(PORT, () => {
+  });
+
+  // Chunk repsonse by cahracter
+  msg.split('').forEach((char, i) =>{
+    setTimeout(() => {
+      res.write(char);
+    }, 500 * i);
+  });
+
+  // Wait for all characters to be sent
+  setTimeout(() => {
+    res.end();
+   },10000);
+
+  });
+
+app.get('/random/:min/:max', (req, res) => {
+  const min = req.params.min;
+  const max = req.params.max;
+  res.send(getRandomInt(+min, +max).toString());
+
+  function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+    }
+  });
+
+app.get('/cal/:month/:year', (req, res) => {
+  const cal = require('node-cal/lib/month');
+  const month = req.params.month;
+  const year = req.params.year;
+  res.end(`${cal.month2Args(month, year)}`);
+  console.log(cal);
+  });
+
+app.all('*',(req,res)=>{
+  res.status(403).send('Access Denied!');
+});
+
+
+app.listen(PORT, () => {
   console.log(`Node.js server has started. Listening on port ${PORT}`);
 });
